6d9adb0162a7352b6f9ef22e1b3c72180602217c
Khathu Musekwa <khathu@vhastech.com>
2022-01-23 12:25:06 +0200

    Post init deployment

--- joker.ts
+++ joker.ts
@@ -2,15 +2,15 @@ import dotenv from 'dotenv';
 import discord from 'discord.js';
 
 dotenv.config();
-// const client = new discord.Client({
-//     intents: 447750720576
-// });
+const client = new discord.Client({
+    intents: 447750720576
+});
 
-// client.on('ready', () => {
-//     console.log(`Logged in as ${client.user.tag}`);
-// });
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
 
-// client.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
 
 const joker = (): void => {
     console.log(`Starting the jokes.`);
