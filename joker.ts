7df9434a983a30a017334f2001941954cf7a324d
Khathu Musekwa <khathu@vhastech.com>
2022-01-23 12:12:02 +0200

    Setup entry point

---
+++ joker.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import discord from 'discord.js';
+
+dotenv.config();
+const client = new discord.Client({
+    intents: 447750720576
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
+
+const joker = (): void => {
+    console.log(`Starting the jokes.`);
+}
+
+setInterval(joker, 3600000);
